0ed359068eca5eb66f4ce5ae22304fd2a6699484
davidcoats <david@getsafetycone.com>
2022-05-03 13:28:33 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0132.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0132.Construction
+{
+    /// <summary>
+    /// Construction project for Marker attribute for functionality classes..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0132/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0132
+{
+    /// <summary>
+    /// Marker attribute for functionality classes.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
